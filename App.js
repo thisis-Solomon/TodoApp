ad8844badd629d583fcf1dc47f27330369150e65
[thisis-Solomon] <[solomonnjobvu1@gmail.com]>
2019-04-01 01:22:05 +0200

    [Added some fuctionality with unabled fn Add To-do btn]

--- App.js
+++ App.js
@@ -1,15 +1,14 @@
 import React, {Component} from 'react';
-import {StyleSheet, Text, View, Button} from 'react-native';
+import {StyleSheet, Text, View, Button, ScrollView} from 'react-native';
 
 let id = 0;
 
 const Todo = props => {
   <View>
-    <input type="checkbox"/>
-    <Button>delete</Button>
-    <span>
+    <Button onPress = {props.onDelete} title = "delete"/>
+    <Text>
       {props.todo.text}
-    </span>
+    </Text>
   </View>
 }
 
@@ -22,9 +21,12 @@ export default class App extends Component{
   }
 
   addTodo(){
-    const text = prompt("Todo text please")
+    id++
+    const text = `TO-DO # ${id}`
     this.setState({
-      todos: [...this.state.todos, {id: id++, text: text}],
+      todos: [
+        ...this.state.todos, {id: id, text: text, checked: false}
+      ],
     })
   }
 
@@ -33,17 +35,34 @@ export default class App extends Component{
       todos: this.state.todo.filter(todo => todo.id !== id)
     })
   }
+
+  toggleTodo(id){
+    this.setState({
+      todos: this.state.todos.map(todo => {
+        if (todo.id !== id) return todo
+        return{
+          id: todo.id,
+          text: todo.text,
+          checked: !todo.checked,
+        }
+
+      })
+    });
+  }
   render() {
     return (
       <View>
-        <Text>
-          <Button onPress = {() => this.addTodo}></Button>
+        <Text>Todo count: {this.state.todos.length}</Text>
+        <Text>Unchecked todo count: {this.state.todos.filter(todo=>  !todo.checked.length)}</Text>
+        <ScrollView>
+          <Button onPress = {() => this.addTodo} title = "Add To-do"/>
           {this.state.todos.map(todo =>(
             <Todo 
+              onToggle = {() => this.toggleTodo(todo.id)}
               onDelete = {() => this.removeTodo(todo.id)} 
               todo= {todo}/>
           ))}
-        </Text>
+        </ScrollView>
       </View>
     );
   }
